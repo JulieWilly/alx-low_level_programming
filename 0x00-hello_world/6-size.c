1fc6ff33af2d2d76fc46dfb9e4367f1f5dd7d358
JulieWillie <logincoin40@gmail.com>
2022-07-01 14:51:26 -0700

    Introduction to low level progamming, C-Hello,world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of char: %d bytes\n", sizeof(char));
+printf("Size of log int: %d bytes\n", sizeof(long int));
+printf("Size of long long int: %d bytes\n", sizeof(long long int));
+return (0);
+}
