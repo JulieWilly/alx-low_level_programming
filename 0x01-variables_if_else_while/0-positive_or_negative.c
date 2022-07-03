af6e9b093418751fcf4afd8eb79c8c42279491a0
JulieWillie <logincoin40@gmail.com>
2022-07-03 13:06:15 -0700

    had to change the error- main has n o decription on this program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there*/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
