e92077d7ae6387a9fcebe301e54fe59cab46f395
JulieWillie <logincoin40@gmail.com>
2022-07-05 16:00:45 -0700

    Printing alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing the  lower case alphabest 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+	char ch;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
