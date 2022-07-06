75b41706f9d168410762e74eb24328c724d7f7f6
JulieWillie <logincoin40@gmail.com>
2022-07-06 16:34:44 -0700

    98 batterly street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints natural numbers upto 98
+ * @n: the beggining of the numbers
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				prinf("%d, ", n);
+			n = n + 1;
+		}
+	}
+	else if (n > 98)
+		{
+			while (n >= 98)
+			{
+				if (n == 98)
+					printf("%d\n", n);
+				else
+					printf("%d, ", n);
+				n = n - 1;
+			}
+		}
+}
