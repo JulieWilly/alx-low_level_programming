47dfa6957493890d60508fa968bea62b5b7e94e5
JulieWillie <logincoin40@gmail.com>
2022-07-07 16:36:48 -0700

    Positive or negative number

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,23 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+	int n;
+	srand(time(0));
+	n = i;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n== 0)
+		printf("%d is zero", n);
+	if (n < 0)
+		printf("%d is negative", n);
+	return (0);
+}
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - debuggin test 1
 * Return: 0
 */
 
