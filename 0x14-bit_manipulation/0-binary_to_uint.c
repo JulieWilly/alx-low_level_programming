aa5dd918d03af862b8d2d11aa8905279fd8ed15c
JulieWilly <logincoin40@gmail.com>
2022-08-19 22:36:18 +0300

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,19 @@
 
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int j = 1;
-	unsigned int k = 0;
-	int c;
-	unsigned int len;
+	unsigned int val = 0;
+	int i = 0;
 
 	if (b == NULL)
-		return (0);
-	len = strlen(b);
-	for (c = len -1; c >= 0; c--)
 	{
-		if (b[c] != '0' && b[c] != '1')
-			return (0);
-		if (b[c] == '1')
-		{
-			k += j;
-		}
-		k *= 2;
+		return (0);
 	}
-	return (k);
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
 }
 
